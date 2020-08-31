cb2a2e6e9e996fc6d7475da046a0004e4aff5d05
dkfagh <dkfagh@gmail.com>
2020-08-31 15:09:55 +0900

    #3.3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
 class App extends React.Component{
+  state = {
+    isLoading : true,
+    movives: []
+  };
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({isLoading : false});
+    }, 6000);
+  }
+
   render(){
-    return <div />
+    const {isLoading} = this.state;
+    return <div>{isLoading ? "Loading..." : "We're ready"}</div>;
   }
 }
 
