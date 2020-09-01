79927431ebb4ad62e93a4820fa3f47e8a34e1938
dkfagh <dkfagh@gmail.com>
2020-09-01 13:39:35 +0900

    Updates

---
+++ precache-manifest.394d7643389f8cbeb4ba3ce9d6474257.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e565bcbf54a2f73bb65c215fe7b97382",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "06c46706b1b97d88b62d",
+    "url": "/movie_app/static/css/main.a9ca4905.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06c46706b1b97d88b62d",
+    "url": "/movie_app/static/js/main.36fd43c1.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
