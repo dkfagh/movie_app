2ab8b555d14519966c17c151898e12596d7d19c0
dkfagh <dkfagh@gmail.com>
2020-09-07 11:27:02 +0900

    #6.2

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from "react";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import { HashRouter, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import "./App.css";
 
 function App() {
    return (
       <HashRouter>
+         <Navigation />
          <Route path="/" exact={true} component={Home} />
          <Route path="/about" component={About} />
       </HashRouter>
