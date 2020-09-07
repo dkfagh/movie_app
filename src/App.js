429caf21e1bdd2c2f2dde83d7f8459a2d924f57a
dkfagh <dkfagh@gmail.com>
2020-09-07 11:11:39 +0900

    #6.1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import { HashRouter, Route } from "react-router-dom";
 
 function App() {
-   return <span>something</span>;
+   return (
+      <HashRouter>
+         <Route path="/" exact={true} component={Home} />
+         <Route path="/about" component={About} />
+      </HashRouter>
+   );
 }
 
 export default App;
